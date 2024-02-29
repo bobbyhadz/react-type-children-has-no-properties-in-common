27f64326de4603fd978fdc7d7faf448ab8cee22e
bobbyhadz <opmftw@gmail.com>
2024-02-29 07:47:40 +0200

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+const Box = () => {
+  return (
+    <div>
+      <p>Hello world</p>
+    </div>
+  );
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <Box />
+    </div>
+  );
+};
+
+export default App;
